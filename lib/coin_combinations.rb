56dd405cb724cc3d8fc0c3df37c2fbc4fbbe5979
michael summers & tony navarro <student@epicodus.com>
2015-08-06 12:22:30 -0700

    Changes coin_combination.rb code to output the correct change string while maintaining correct spacing.

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -1,33 +1,53 @@
 class String
   define_method(:coin_count) do
-    coin_count = [0, 0, 0, 0]
+    coin_count = [0,0,0,0]
 
     coin = self.to_i
+    output_str = ""
+    first_count = true
 
     #quarters loop
     until coin. < 25 do
       coin_count[0] += 1
       coin -= 25
+      first_count = false
     end
-    #coin_count
+    if coin_count[0] > 0
+      output_str = coin_count[0].to_s + " quarters "
+    end
+
     #dimes loop
     until coin. < 10 do
       coin_count[1] = coin_count[1] + 1
       coin -= 10
+      first_count = false
     end
+    if coin_count[1] > 0
+      output_str = output_str + coin_count[1].to_s + " dimes "
+    end
+
     #nickles loop
     until coin. < 5 do
      coin_count[2] += 1
      coin -= 5
+     first_count = false
     end
+    if coin_count[2] > 0
+      output_str = output_str + coin_count[2].to_s + " nickles "
+    end
+
     #pennies loop
     until coin. == 0 do
      coin_count[3] += 1
      coin -= 1
+     first_count = false
     end
-
-    output_str = coin_count[0].to_s + "quarters" + coin_count[1].to_s + "dimes" + coin_count[2].to_s + "nickles" + coin_count[3].to_s + "pennies"
-
+    if coin_count[3] > 0
+      output_str = output_str + coin_count[3].to_s + " pennies"
+    end
+    if output_str[0] == " "
+      output_str[0] = ''
+    end
     output_str
   end
 end
--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -4,6 +4,20 @@ require('pry')
 
 describe('String#coin_count') do
   it('inputs string of "change" you want converted and returns coin totals as string') do
-  expect(("10").coin_count()).to(eq("0quarters1dimes0nickles0pennies"))
+  expect(("10").coin_count()).to(eq("1 dimes "))
+  end
+  it('inputs string "change" worth multiple coins you want converted and returns coin totals') do
+  expect(("91").coin_count()).to(eq("3 quarters 1 dimes 1 nickles 1 pennies"))
   end
 end
+
+
+# 91
+#
+# 3 = 75
+#
+# 1 = 85
+#
+# 1 = 90
+#
+# 1 = 91
